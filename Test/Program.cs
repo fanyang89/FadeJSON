dea89922cf2ac97f32bb8a2df610721a76825819
fuis <fuis@qq.com>
2015-08-12 18:37:10 +0800

    测试中添加与json.net的比较

--- Test/Program.cs
+++ Test/Program.cs
@@ -5,15 +5,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FadeJson;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
 
 namespace Test
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args) {
             var content = File.ReadAllText("test.json");
+            var stopwatch = new Stopwatch();
 
+            stopwatch.Start();
             var j = Json.Parse(content);
+            stopwatch.Stop();
+            Console.WriteLine("FadeJson: {0}ms", stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            var jobj = JObject.Parse(content);
+            stopwatch.Stop();
+            Console.WriteLine("Newtonsoft.Json: {0}", stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
             Console.ReadKey();
         }
     }
