fd373702c77eea9fe76602311e9c4e4cba8a27de
YangFan789 <YangFan789@outlook.com>
2015-09-06 14:37:02 +0800

    完成导出实体类的工具

--- src/FadeJson-dotNet46/FadeJson.Toolkit/Program.cs
+++ src/FadeJson-dotNet46/FadeJson.Toolkit/Program.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using System.Text;
 using System.CodeDom;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
 
-namespace FadeJson.Toolkit
+namespace FadeJson.Toolkit.ExportClass
 {
-    static class Program
+    internal static class Program
     {
         //输入：json格式的文件内容
         //输出：该文件对应的类(*.cs)
 
-        static void Main(string[] args) {
-            if (args.Length != 1) {
-                Console.WriteLine("Invalid args counts. Just one argument is accepted.");
-                return;
-            }
-            fileName = args[0];
-            var j = JsonValue.FromFile(fileName);
-            ParserObj(j, "ROOT");
-            Console.WriteLine("Done.");
+        private static int classCount = 0;
+
+        private static string GetNameFromEnum(JsonValueType valType) {
+            var typeNames = new[] { "JsonValue", "JsonValue", "int", "string", "bool" };
+            return typeNames[(int)valType];
         }
 
-        static string Join(this List<string> list) {
+        private static string Input(string msg) {
+            Console.WriteLine(msg);
+            return Console.ReadLine();
+        }
+
+        private static string Join(this List<string> list) {
             var sb = new StringBuilder();
             foreach (var str in list) {
                 sb.Append(str);
@@ -33,29 +34,77 @@ namespace FadeJson.Toolkit
             return sb.ToString();
         }
 
-        static string fileName = string.Empty;
-        static int classCount = 0;
+        private static void Main(string[] args) {
+            string namespaceName = string.Empty;
+            string className = string.Empty;
+            string fileName = string.Empty;
 
-        static void ParserObj(JsonValue j, string className) {
-            var list = new List<string>();
-            list.Add($"public class {className} {{");
-            foreach (var key in j.Keys) {
-                if (j[key].JsonValueType == JsonValueType.Object) {
-                    ParserObj(j[key], $"{className}{classCount}");
-                    list.Add($"public {className}{classCount} {key.ToString()} {{ get; set; }}");
-                    continue;
-                }
-                var typeName = GetNameFromEnum(j[key].JsonValueType);
-                list.Add($"public {typeName} {key.ToString()} {{ get; set; }}");
+            Console.WriteLine("FadeJson.ToolKit.ExportClass: 帮助你从JSON文件生成对应的类");
+
+            if (args.Length != 1 && args.Length != 3) {
+                Console.WriteLine("Invalid Arguments.");
+                Console.WriteLine("Drag json file to this exe OR use it in cmd like this");
+                Console.WriteLine("FadeJson.Toolkit.ExportClass.exe test.Json myNamespace myClass");
+                Console.ReadKey();
+                return;
             }
-            list.Add("}");
 
-            File.WriteAllText($"{Path.GetFileNameWithoutExtension(fileName)}-{classCount++}.cs", list.Join());
+            fileName = args[0];
+
+            if (args.Length == 1) {
+                namespaceName = Input("NamespaceName is ?");
+                className = Input("ClassName is ?");
+            }
+
+            if (args.Length == 3) {
+                namespaceName = args[1];
+                className = args[2];
+            }
+
+            GenerateTypeDecl(JsonValue.FromFile(fileName), namespaceName, className);
+            Console.WriteLine("Done.");
         }
 
-        static string GetNameFromEnum(JsonValueType valType) {
-            var typeNames = new[] { "JsonValue", "JsonValue", "int", "string", "bool" };
-            return typeNames[(int)valType];
+        private static void GenerateTypeDecl(JsonValue j, string namespaceName, string className) {
+            var codeNamespace = new CodeNamespace(namespaceName);
+            codeNamespace.Imports.Add(new CodeNamespaceImport("FadeJson"));
+            var codeClass = new CodeTypeDeclaration(className);
+            var codeCtor = new CodeConstructor();
+            codeCtor.Attributes = MemberAttributes.Public;
+            codeCtor.Name = className;
+            codeCtor.Parameters.Add(new CodeParameterDeclarationExpression("JsonValue", "j"));
+
+            foreach (var key in j.Keys) {
+                var o = j[key];
+                if (o.JsonValueType == JsonValueType.Object) {
+                    var newClassName = $"{className}{classCount++}";
+                    GenerateTypeDecl(o, namespaceName, newClassName);
+                    var newProperty = new CodeSnippetTypeMember(
+                        $"        public {newClassName} {key.ToString()} {{ get; set; }}");
+                    codeClass.Members.Add(newProperty);
+                    codeCtor.Statements.Add(new CodeSnippetStatement(
+                        $"            {key.ToString()} = new {newClassName}(j[\"{key.ToString()}\"]);"
+                        ));
+                    continue;
+                }
+                var typeName = GetNameFromEnum(o.JsonValueType);
+                var newField = new CodeSnippetTypeMember(
+                    $"        public {typeName} {key.ToString()} {{ get; set; }}");
+                codeClass.Members.Add(newField);
+                codeCtor.Statements.Add(new CodeSnippetStatement(
+                    $"            {key.ToString()} = ({typeName})j[\"{key.ToString()}\"].Value;"));
+            }
+
+            codeClass.Members.Add(codeCtor);
+            codeNamespace.Types.Add(codeClass);
+            var codeGenerater = new CSharpCodeProvider();
+            var stringWriter = new StringWriter();
+            codeGenerater.GenerateCodeFromNamespace(codeNamespace, stringWriter, new CodeGeneratorOptions() {
+                VerbatimOrder = true,
+                BlankLinesBetweenMembers = true,
+                BracingStyle = "C"
+            });
+            File.WriteAllText(className + ".cs", stringWriter.ToString());
         }
     }
 }
